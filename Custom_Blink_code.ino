6bd52adafe2ff0ee463708560f609bccec91cf42
Grid21 <minilife92@gmail.com>
2013-05-11 09:54:30 -0400

    Made a time change.

--- Custom_Blink_code.ino
+++ Custom_Blink_code.ino
@@ -6,18 +6,20 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(7, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(7, LOW);
+  delay(800);
+  digitalWrite(7, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(500);               // wait for a second
   digitalWrite(7, LOW);    // turn the LED off by making the voltage LOW
-  delay(900);                // wait for a second
+  delay(800);                // wait for a second
   digitalWrite(7, HIGH);
   delay(500);
   digitalWrite(7, LOW);
-  delay(900);
+  delay(800);
   digitalWrite(7, HIGH);
   delay(500);
   digitalWrite(7, LOW);
-  delay(900);
+  delay(800);
   digitalWrite(7, HIGH);
-  delay(8000);
+  delay(1000);
 }
